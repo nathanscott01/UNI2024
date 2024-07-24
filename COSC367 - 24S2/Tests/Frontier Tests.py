71c06260cd61a92f1e5edb1a70f1da98df9b8dde
Nathan <scotty05nj@gmail.com>
2024-07-24 23:02:36 +1200

    FunkyNumeric and SlidingPuzzleGraph test cases and code uploaded, need solved

--- COSC367 - 24S2/Tests/Frontier Tests.py
+++ COSC367 - 24S2/Tests/Frontier Tests.py
@@ -1,6 +1,9 @@
 import io
 import unittest
+
+from bfs_frontier import BFSFrontier
 from dfs_frontier import *
+
 from contextlib import redirect_stdout
 
 # Input Graphs
@@ -44,6 +47,10 @@ expected_3 = """Actions:
   Auckland->Gold Coast.
 Total cost: 3"""
 
+expected_4 = """Actions:
+  Christchurch->Gold Coast.
+Total cost: 1"""
+
 
 class MyTestCase(unittest.TestCase):
     def test_dfs_frontier_1(self):
@@ -71,6 +78,23 @@ class MyTestCase(unittest.TestCase):
             print_actions(my_itinerary)
         output = f.getvalue().strip()
         self.assertEqual(expected_3, output)
+
+    def test_bfs_frontier_1(self):
+        f = io.StringIO()
+        with redirect_stdout(f):
+            solutions = generic_search(graph1, BFSFrontier())
+            solution = next(solutions, None)
+            print_actions(solution)
+        output = f.getvalue().strip()
+        self.assertEqual(expected_1, output)
+
+    def test_bfs_frontier_2(self):
+        f = io.StringIO()
+        with redirect_stdout(f):
+            my_itinerary = next(generic_search(available_flights, BFSFrontier()), None)
+            print_actions(my_itinerary)
+        output = f.getvalue().strip()
+        self.assertEqual(expected_4, output)
 
 
 if __name__ == '__main__':
