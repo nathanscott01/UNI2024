d6a8aefe03f9772625053c758cd8382c84122b74
Nathan <scotty05nj@gmail.com>
2024-08-09 01:17:34 +1200

    Lab 3 working through, need to implement forward_deduce

---
+++ COSC367 - 24S2/forward_deduce.py
@@ -0,0 +1,51 @@
+"""
+Nathan Scott
+COSC367 Lab 3
+Forward Deduce
+"""
+
+import re
+
+
+def clauses(knowledge_base):
+    """Takes the string of a knowledge base; returns an iterator for pairs
+    of (head, body) for propositional definite clauses in the
+    knowledge base. Atoms are returned as strings. The head is an atom
+    and the body is a (possibly empty) list of atoms.
+
+    -- Kourosh Neshatian - 2 Aug 2021
+
+    """
+    ATOM   = r"[a-z][a-zA-Z\d_]*"
+    HEAD   = rf"\s*(?P<HEAD>{ATOM})\s*"
+    BODY   = rf"\s*(?P<BODY>{ATOM}\s*(,\s*{ATOM}\s*)*)\s*"
+    CLAUSE = rf"{HEAD}(:-{BODY})?\."
+    KB     = rf"^({CLAUSE})*\s*$"
+
+    assert re.match(KB, knowledge_base)
+
+    for mo in re.finditer(CLAUSE, knowledge_base):
+        yield mo.group('HEAD'), re.findall(ATOM, mo.group('BODY') or "")
+
+
+def forward_deduce(knowledge_base):
+    """Return a strign of atoms that can be derived to be true from knowledge base."""
+
+
+# kb = """
+# a :- b.
+# b.
+# """
+
+kb = """
+a :- b, c.
+b :- d, e.
+b :- g, e.
+c :- e.
+d.
+e.
+f :- a,
+     g.
+"""
+
+print(list(clauses(kb)))
