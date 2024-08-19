cd7c5401a1b433ec5138036dbeb2d0a61a0b2972
Nathan Scott <scotty05nj@gmail.com>
2024-08-19 18:04:34 +1200

    Still bashing away at arc-consistency

--- COSC367 - 24S2/CSP Lab/arc_consistent.py
+++ COSC367 - 24S2/CSP Lab/arc_consistent.py
@@ -7,25 +7,37 @@ CSP Arc Consistent
 import itertools, copy
 from csp import *
 
+
 def arc_consistent(csp):
     csp = copy.deepcopy(csp)
-    to_do = {(x, c) for c in csp.constraints for x in scope(c)} # COMPLETE
+    to_do = {(x, c) for c in csp.constraints for x in scope(c)}  # COMPLETE
     while to_do:
         x, c = to_do.pop()
         ys = scope(c) - {x}
         new_domain = set()
-        for xval in csp.var_domains[x]: # COMPLETE
+        for xval in csp.var_domains[x]:  # COMPLETE
             assignment = {x: xval}
             for yvals in itertools.product(*[csp.var_domains[y] for y in ys]):
                 assignment.update({y: yval for y, yval in zip(ys, yvals)})
-                if (satisfies(assignment, c) for c in csp.constraints): # COMPLETE
-                    new_domain.add(assignment) # COMPLETE
+                if (satisfies(assignment, c) for c in csp.constraints):  # COMPLETE
+                    new_domain.add(assignment[x])  # COMPLETE
                     break
         if csp.var_domains[x] != new_domain:
             for cprime in set(csp.constraints) - {c}:
                 if x in scope(cprime):
-                   for z in scope(cprime): # COMPLETE
-                       if x != z: # COMPLETE
-                           to_do.add((z, cprime))
-            csp.var_domains[x] = new_domain     #COMPLETE
+                    for z in scope(cprime):  # COMPLETE
+                        if x != z:  # COMPLETE
+                            to_do.add((z, cprime))
+            csp.var_domains[x] = new_domain  # COMPLETE
     return csp
+
+simple_csp = CSP(
+    var_domains={x: set(range(1, 5)) for x in 'abc'},
+    constraints={
+        lambda a, b: a < b,
+        lambda b, c: b < c,
+        })
+
+csp = arc_consistent(simple_csp)
+for var in sorted(csp.var_domains.keys()):
+    print("{}: {}".format(var, sorted(csp.var_domains[var])))
