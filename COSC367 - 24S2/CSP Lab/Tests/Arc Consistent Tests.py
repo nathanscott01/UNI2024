a9f09dc4b6abc1a48af62d1f78821ef5ec15b171
Nathan <scotty05nj@gmail.com>
2024-08-19 16:01:53 +1200

    Working on GAC

---
+++ COSC367 - 24S2/CSP Lab/Tests/Arc Consistent Tests.py
@@ -0,0 +1,49 @@
+import unittest
+import io
+from contextlib import redirect_stdout
+from arc_consistent import *
+
+# Inputs
+simple_csp = CSP(
+    var_domains={x: set(range(1, 5)) for x in 'abc'},
+    constraints={
+        lambda a, b: a < b,
+        lambda b, c: b < c,
+        })
+
+
+csp2 = CSP(var_domains={x:set(range(10)) for x in 'abc'},
+          constraints={lambda a,b,c: 2*a+b+2*c==10})
+
+# Expected Outputs
+expected1 = """a: [1, 2]
+b: [2, 3]
+c: [3, 4]
+"""
+
+expected2 = """a: [0, 1, 2, 3, 4, 5]
+b: [0, 2, 4, 6, 8]
+c: [0, 1, 2, 3, 4, 5]"""
+
+
+class MyTestCase(unittest.TestCase):
+    def test_simple(self):
+        f = io.StringIO()
+        with redirect_stdout(f):
+            csp = arc_consistent(simple_csp)
+            for var in sorted(csp.var_domains.keys()):
+                print("{}: {}".format(var, sorted(csp.var_domains[var])))
+        output = f.getvalue().strip()
+        self.assertEqual(expected1, output)
+
+    def test_crossword(self):
+        f = io.StringIO()
+        with redirect_stdout(f):
+            csp = arc_consistent(csp2)
+            for var in sorted(csp.var_domains.keys()):
+                print("{}: {}".format(var, sorted(csp.var_domains[var])))
+        output = f.getvalue().strip()
+        self.assertEqual(expected2, output)
+
+if __name__ == '__main__':
+    unittest.main()
--- COSC367 - 24S2/CSP Lab/generate_and_test.py
+++ COSC367 - 24S2/CSP Lab/generate_and_test.py
@@ -10,6 +10,24 @@ import itertools
 def generate_and_test(csp):
     names, domains = zip(*csp.var_domains.items())
     for values in itertools.product(*domains):
-        assignment = {#COMPLETE for x, v in zip(names, values)}
-        if all(satisfies(#COMPLETE):
+        assignment = {x : v for x, v in zip(names, values)}
+        if all(satisfies(assignment, constraint) for constraint in csp.constraints):
             yield assignment
+
+
+
+canterbury_colouring = CSP(
+    var_domains={
+        'christchurch': {'red', 'green'},
+        'selwyn': {'red', 'green'},
+        'waimakariri': {''},
+        },
+    constraints={
+        lambda christchurch, waimakariri: christchurch != waimakariri,
+        lambda christchurch, selwyn: christchurch != selwyn,
+        lambda selwyn, waimakariri: selwyn != waimakariri,
+        })
+
+solutions = sorted(str(sorted(solution.items())) for solution
+                   in generate_and_test(canterbury_colouring))
+print("\n".join(solutions))
