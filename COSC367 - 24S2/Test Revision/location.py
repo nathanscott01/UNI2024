1892e22d3173079ebc0e05b6cf4e987639d3a4b9
Nathan Scott <scotty05nj@gmail.com>
2024-09-19 13:47:38 +1200

    Location and LCFS Frontier enabled and implemented

--- COSC367 - 24S2/Test Revision/location.py
+++ COSC367 - 24S2/Test Revision/location.py
@@ -24,23 +24,37 @@ class LocationGraph(Graph):
 
     def outgoing_arcs(self, tail):
         outgoing = []
-        x1, y1 = self.location(tail)
-        for node in self.location:
-            if node != tail:
-                x2, y2 = self.location(node)
-                if sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2) <= 5:
-                    outgoing.append(node)
-        return outgoing
+        x1, y1 = self.location[tail]
+        for head in self.location:
+            if head != tail:
+                x2, y2 = self.location[head]
+                distance = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+                if distance <= self.radius:
+                    outgoing.append(Arc(tail, head, str(tail) + '->' + str(head), distance))
+        return sorted(outgoing, key=lambda arc: arc.head)
 
 
 class LCFSFrontier(Frontier):
     def __init__(self):
         self.container = []
+        self.counter = 0
+        self.node_tracker = {}
 
     def add(self, path):
-        heapq.heappush()
+        total_cost = sum(arc.cost for arc in path)
+        head = path[-1].head
+        if head is None:
+            heapq.heappush(self.container, (total_cost, self.counter, path))
+            self.counter += 1
+        elif head not in self.node_tracker or total_cost < self.node_tracker[head]:
+            heapq.heappush(self.container, (total_cost, self.counter, path))
+            self.counter += 1
+            self.node_tracker[head] = total_cost
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        if not self.container:
+            raise StopIteration
+        return heapq.heappop(self.container)[2]
